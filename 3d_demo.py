WIP: Fixes for 3D code. Equation mistakes. Error in computation of y

3d_demo.py
@@ -38,9 +38,9 @@ def get_polygon_normal(polygon):
 
 def edge_contribution(e, r_0, r_1, x, y, ell):
     # This guy is scalar
-    E = log(r_1 + ell - x / (r_0 - x))
-    F = (y**2 + e*r_0 / (r_0 + e))**2 + (y**2 + e*r_1 / (r_1 + e))**2 - y**2
-    H = sqrt(y**2 - e**2) * (y**2*ell + e*(ell - x)*r_0 + e*x*r_1) / (y**2*(r_0 + e)*(r_1 + e))
+    E = log((r_1 + ell - x) / (r_0 - x))
+    F = ((y**2 + e*r_0) / (r_0 + e))**2 + ((y**2 + e*r_1) / (r_1 + e))**2 - y**2
+    H = (sqrt(y**2 - e**2) * (y**2*ell + e*(ell - x)*r_0 + e*x*r_1)) / (y**2*(r_0 + e)*(r_1 + e))
     if F > 0:
         beta = asin(H)
     else:
@@ -51,6 +51,7 @@ def edge_contribution(e, r_0, r_1, x, y, ell):
 
 def L(polygon, r, omega):
     n = get_polygon_normal(polygon)
+    assert isclose(np.linalg.norm(n), 1.0)
     e_n = n
     if np.dot(e_n, r) < 0.0:
         e_n = -e_n
@@ -61,18 +62,30 @@ def L(polygon, r, omega):
     for vertex_0, vertex_1 in pairwise(polygon):
         ell = np.linalg.norm(vertex_1 - vertex_0)
         s_i = (vertex_1 - vertex_0) / ell
+        assert isclose(np.linalg.norm(s_i), 1.0)
         r_0 = vertex_0 - r
         r_1 = vertex_1 - r
         e = np.dot(e_n, r_0)
+        k = np.cross(s_i, r_0)
+        k /= np.linalg.norm(k)
         x = -np.dot(r_0, s_i)
-        y = np.dot(np.cross(r_0, s_i), n)
+        y =  np.dot(r_0, np.cross(s_i, k))
+        # print(f'r_0={r_0}, y={y}, e={e}')
+        assert abs(e) <= abs(y)
+        # It is just a coordinate transformation it should still have the same length
+        assert isclose(sqrt(x**2 + y**2), np.linalg.norm(r_0))
         K_1 = edge_contribution(e, np.linalg.norm(r_0), np.linalg.norm(r_1), x, y, ell)
         b_i = np.dot(np.cross(n, r), s_i)
         K_1_sum += b_i * K_1
     return gamma * K_1
     
     
-#def field_for_volume(polygons, )
+def field_for_volume(polygons, r, omega):
+    field = np.zeros(3)
+    for polygon in polygons:
+        field += L(polygon, r, omega)
+    return -field / (4*pi)
+
 
 vertices = np.array([
     [-1.0, -1.0, -1.0],
@@ -88,17 +101,34 @@ vertices = np.array([
 
 CURRENT_DENSITY = np.array([0.0, 0.0, 2.0])
 face1 = np.array([vertices[0], vertices[1], vertices[2], vertices[3], vertices[0]])
-face2 = [vertices[4], vertices[5], vertices[6], vertices[7], vertices[4]]
-face3 = [vertices[0], vertices[1], vertices[5], vertices[4], vertices[0]]
-face4 = [vertices[2], vertices[3], vertices[7], vertices[6], vertices[2]]
-face5 = [vertices[1], vertices[2], vertices[6], vertices[5], vertices[1]]
-face6 = [vertices[0], vertices[3], vertices[7], vertices[4], vertices[0]]
+face2 = np.array([vertices[4], vertices[5], vertices[6], vertices[7], vertices[4]])
+face3 = np.array([vertices[0], vertices[1], vertices[5], vertices[4], vertices[0]])
+face4 = np.array([vertices[2], vertices[3], vertices[7], vertices[6], vertices[2]])
+face5 = np.array([vertices[1], vertices[2], vertices[6], vertices[5], vertices[1]])
+face6 = np.array([vertices[0], vertices[3], vertices[7], vertices[4], vertices[0]])
 box = [face1, face2, face3, face4, face5, face6]
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.add_collection3d(Poly3DCollection(box, alpha=0.2))
+#fig = plt.figure()
+#ax = fig.add_subplot(projection='3d')
+#ax.add_collection3d(Poly3DCollection(box, alpha=0.2))
 #plt.show()
 
 r = np.array([-5.0, 0.0, 5.0])
-L(face1, r, CURRENT_DENSITY)
+print(field_for_volume(box, r, CURRENT_DENSITY))
+
+
+## Plotting
+X = np.linspace(-2.5, 2.5, 50)
+Y = np.linspace(-2.5, 2.5, 50)
+x_grid, y_grid = np.meshgrid(X, Y)
+
+np_field = np.vectorize(lambda x_p, y_p: field_for_volume(box, np.array([x_p, y_p, 0.0]), CURRENT_DENSITY), signature='(),()->(n)')
+B = np_field(x_grid, y_grid)
+B_x = B[:, :, 0]
+B_y = B[:, :, 1]
+
+plt.rcParams["figure.figsize"] = (12, 10)
+plt.streamplot(x_grid, y_grid, B_x, B_y)
+plt.contourf(x_grid, y_grid, (B_x**2 + B_y**2)**0.5)
+plt.colorbar()
+plt.show()